5df5450417e9743e80b9ba170c7dc2f681b44a9b
thiago <thiago@blockchain.com>
2018-12-22 15:25:15 +0000

    application plugin for kraft-cli

--- cli/src/main/kotlin/net/tvidal/kraft/LocalTest.kt
+++ cli/src/main/kotlin/net/tvidal/kraft/LocalTest.kt
@@ -1,5 +1,11 @@
 package net.tvidal.kraft
 
+import net.tvidal.kraft.logging.KRaftLogger
+import java.util.logging.Level
+
 fun main(args: Array<String>) {
     logbackConfigurationFile = LOGBACK_CONSOLE
+
+    val log = KRaftLogger {}
+    log.warn { Level.SEVERE }
 }
